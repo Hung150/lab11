a1862de9d2179da394bb77582fb6605512af78ff
Hung150 <55206412+Hung150@users.noreply.github.com>
2019-10-10 09:23:11 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+	int a,b,sum=0;
+	cout<<"Enter length a ; cin>>a;
+	cout<<"Enter length b ; cin>>b;
+	while(a-sum>=b)
+		sum+=b;
+	printf("the length of the unoccupied part of segment a is %i",a-sum);
+}
